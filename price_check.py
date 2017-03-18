0dc3e9d70f8e194b589cddb75f0867430edabbc6
Sorlas <github@skirner.de>
2017-03-18 16:26:27 +0000

    Almost complete rewrite
    - Price checker can now check multiple sources in one go
    - The history file can now be set for each source in the config. NONE as
    configname skips history creation

--- price_check.py
+++ price_check.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-
 import os
 import re
 import json
@@ -15,32 +14,91 @@ from lxml import html
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-def send_email(price, url, email_info, article):
+def send_email(price, url, article, email_credentials):
     try:
-        s = smtplib.SMTP_SSL(email_info['smtp_url'])
-        #s.starttls()
-        s.login(str(email_info['user']), str(email_info['password']))
+        s = smtplib.SMTP_SSL(email_credentials['smtp_url'])
+        s.login(str(email_credentials['user']), str(email_credentials['password']))
     except smtplib.SMTPAuthenticationError:
         print('Failed to login')
     except Exception:
-        print('Someting Wong')
+        print('Someting Wong with Email sending')
     else:
         print('Logged in! Composing message..')
         msg = MIMEMultipart('alternative')
-        msg['Subject'] = 'Price Alert - %s' % price
-        msg['From'] = email_info['user']
-        msg['To'] = email_info['user']
-        text = '%s price has a new all time low!\nThe price is currently %s !! URL to salepage: %s' % (
-            article, price, url)
+        msg['Subject'] = '%s Price Alert - %s' % (article, price)
+        msg['From'] = email_credentials['user']
+        msg['To'] = email_credentials['user']
+        text = '%s price has a new all time low!\nThe price is currently %s !! URL to salepage: %s' % (article, price, url)
         part = MIMEText(text, 'plain')
         msg.attach(part)
-        s.sendmail(str(email_info['user']), str(email_info['user']), msg.as_string())
+        s.sendmail(str(email_credentials['user']), str(email_credentials['user']), msg.as_string())
         print('Message has been sent.')
 
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config',default='%s/config.json' % os.path.dirname(os.path.realpath(__file__)), help='Configuration file')
+    parser.add_argument('-p', '--poll-interval', type=int, default=3600, help='Time in seconds between checks, default 1 hour')
+    parser.add_argument('-r', '--random-poll', type=int, default=0, help='Random wait time between 0 and input to add to poll-interval')
+    parser.add_argument('-e', '--endless', action='store_true', help='Endless Run')
+    return parser.parse_args()
 
-def get_price(url, regex, listid):
+def read_config(config_file):
+    try:
+        with open(config_file, 'r') as f:
+            return json.loads(f.read())
+    except Exception:
+        print('Failed to open config file')
+        exit()
+
+def write_config(config_file, data):
+    try:
+        with open(config_file, 'w') as f:
+            json.dump(data, f, indent=4, sort_keys=True)
+    except Exception:
+        print('failed to write config file!')
+
+def wait(random, waittime):
+    #No unsigned datatype, so just convert any negative to positive
+    if(random < 0):
+        random *= -1
+        print('Negative random wait time given, convert to positive')
+    if(waittime < 0):
+        waittime *= -1
+        print('Negative wait time given, convert to positive')
+
+    random_wait_time = randint(0,random)
+    random_wait_time += waittime
+    print('Sleeping for', random_wait_time, 'seconds')
+    time.sleep(random_wait_time)
+
+def update_items(items, email_credentials):
+    prices_updated = False
+    for item in items:
+        current_regex = item['regex']
+        current_listid = item['listid']
+        current_items = item['item']
+        current_historyfile = item['historyfile']
+
+        for each_item in current_items:
+            print('Checking', each_item[2])
+            current_price = get_price(each_item[0], current_regex, current_listid)
+            if (current_price < each_item[1]):
+                print('New price is', current_price, '!!')
+                each_item[1] = current_price
+                prices_updated = True
+                send_email(current_price, each_item[0], each_item[2], email_credentials)
+            else:
+                print('Price is %.2f not lower than before...' % current_price)
+            if(current_historyfile != 'NONE'):
+                write_history(current_historyfile, current_price, each_item[2])
+            else:
+                print('No history file requested, omitting...')
+    return prices_updated
+
+
+def get_price(URL, regex, regex_price_id):
     #Get Wegpage content
-    r = requests.get(url, headers={
+    r = requests.get(URL, headers={
         'User-Agent':
             'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 '
             '(KHTML, like Gecko) Chrome/52.0.2743.82 Safari/537.36'
@@ -50,103 +108,56 @@ def get_price(url, regex, listid):
     htmlfile = r.text
 
     price_string = re.findall(regex, htmlfile)
+
     try:
-        if not type(listid) is int:
+        if not type(regex_price_id) is int:
             raise TypeError
         else:
-            if listid < 0:
+            if regex_price_id < 0:
                 raise ValueError
     except TypeError:
-        print('ListID is not of type Integer')
+        print('regex_price_id is not of type Integer')
         return 0
     except ValueError:
-        print('ListID must be >= 0')
+        print('regex_price_id must be >= 0')
         return 0
     except Exception:
-        print('Unhandled error with ListID')
+        print('Unhandled error with regex_price_id')
         return 0
 
+    #If Object is just a list
     try:
-        price_string = price_string[0][listid]
-        price = float(price_string.replace(',', '.'))
+        price = price_string[regex_price_id]
+        price = float(price.replace(',', '.'))
         return price
     except Exception:
-        print("ERROR: Cannot find price string")
-        return 0
+        #If object is a tuple in a list. Thats the case when re.findall() returns multiple results
+        try:
+            price = price_string[0][regex_price_id]
+            price = float(price.replace(',', '.'))
+            return price
+        except Exception:
+            print("ERROR: Cannot find price string")
+            return 0
 
+def write_history(history_file, price, itemname):
+    current_date = time.strftime("%d/%m/%Y")
+    current_time = time.strftime("%H:%M:%S")
 
-def get_config(config):
-    with open(config, 'r') as f:
-        return json.loads(f.read())
-
-
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--config',default='%s/config.json' % os.path.dirname(os.path.realpath(__file__)), help='Configuration file path')
-    parser.add_argument('-t', '--poll-interval', type=int, default=3600, help='Time in seconds between checks, default 1 hour')
-    parser.add_argument('-r', '--random-poll', action='store_true', help='Adds 1 to 10 min to poll intervall randomly')
-    parser.add_argument('-o', '--outputfile', default='%s/price_history.txt' % os.path.dirname(os.path.realpath(__file__)), help='History filename for history data, default = price_history.txt')
-    parser.add_argument('-e', '--endless', action='store_true', help='Endless Run')
-    return parser.parse_args()
-
-def write_config(config, data):
-    with open(config, 'w') as f:
-        json.dump(data, f, indent=4, sort_keys=True)
-
-def compare_prices(items, config, history_file):
-    prices_updated = False
-
-    for item in copy(items):
-        print('Checking price for %s (should be lower than %s)' % (item[2], item[1]))
-        item_page = item[0]
-        price = get_price(item_page, config['regex'], config['listid'])
-
-        if(history_file != "NONE"):
-            price_file = open(history_file, 'a')
-            price_file.write("%s;%s;%s;%s\n" % (time.strftime("%d/%m/%Y"), time.strftime("%H:%M:%S"), price, str(item[2])))
-
-        if not price:
-            continue
-        elif price < item[1]:
-            print('Price is %s!! Trying to send email.' % price)
-            send_email(price, item_page, config['email'], item[2])
-            print('Updating config for new price!')
-            item[1] = price
-            prices_updated = True
-        else:
-            print('Price is %s. Ignoring...' % price)
-
-        if(history_file != "NONE"):
-            price_file.close()
-    return prices_updated
-
-def wait(poll_interval, random=False):
-    if random:
-        random_number = randint(60,600)
-        random_number += poll_interval
-    else:
-        random_number = poll_interval
-    print('Sleeping for %d seconds' % random_number)
-    time.sleep(random_number)
+    with open(history_file, 'a') as history:
+        history.write("%s;%s;%s;%s\n" % (current_date, current_time, price, itemname))
 
 def main():
-    args = parse_args()
-    config = get_config(args.config)
-    items = config['items']
+    args = parse_arguments()
+    config = read_config(args.config)
 
     if args.endless:
-        while True and len(items):
-            if compare_prices(items, config, args.outputfile):
+        while(True):
+            if(update_items(config['items'], config['email'])):
                 write_config(args.config, config)
-            wait(args.poll_interval, args.random_poll)
+            wait(args.random_poll, args.poll_interval)
     else:
-        if compare_prices(items, config, args.outputfile):
-            write_config(args.config, config)
-
-    print('Run completed, exiting')
-
+        update_prices(config)
 
 if __name__ == '__main__':
-	#Necessary because started with system.d. Otherwise crash because of no available ethernet
-	#time.sleep(10)
 	main()
